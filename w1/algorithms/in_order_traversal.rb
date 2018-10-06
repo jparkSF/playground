d420521c149d958ef0a55e6886fb3b8e71dde11b
jpark830@me.com <jpark830@me.com>
2018-10-05 18:25:38 -0700

    inorder traversal without recursion

--- w1/algorithms/in_order_traversal.rb
+++ w1/algorithms/in_order_traversal.rb
@@ -4,4 +4,28 @@ def inorder_traversal_with_recursion(root)
     inorder_traversal_with_recursion(root.left)
     print root.val
     inorder_traversal_with_recursion(root.right)
-end
+end 
+
+
+
+def inorder_traversal_without_recursion(root)
+  stack = []
+  output = []
+  current = root
+
+  while true 
+    if !current.nil?
+      stack.push(current)
+      current = current.left
+    else 
+      if stack.empty?
+        break
+      else 
+        current = stack.pop
+        output.push(current)
+        current = current.right
+    end 
+  end 
+
+  output
+end 
