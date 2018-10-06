b7062e799416c50206ae66df556942d8db5f01d0
jpark830@me.com <jpark830@me.com>
2018-10-05 21:16:15 -0700

    add Edge class with initializer

--- w1/algorithms/graph.rb
+++ w1/algorithms/graph.rb
@@ -7,3 +7,16 @@ class Vertex
     @out_edges = []
   end
 end
+
+class Edge
+  attr_accessor :cost, :from_vertex, :to_vertex
+
+  def initialize(from_vertex, to_vertex, cost = 1)
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @from_vertex.out_edges.push(self)
+    @to_vertex.in_edges.push(self)
+    @cost = cost
+  end
+
+end
