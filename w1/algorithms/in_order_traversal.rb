b362483478fbeadee3a2a407bb3aa2c35b3879e2
jpark830@me.com <jpark830@me.com>
2018-10-05 18:22:29 -0700

    inorder traversal with recursion

---
+++ w1/algorithms/in_order_traversal.rb
@@ -0,0 +1,8 @@
+def inorder_traversal_with_recursion(root)
+   return if root.nil?
+
+    inorder_traversal_with_recursion(root.left)
+    print root.val
+    inorder_traversal_with_recursion(root.right)
+end 
+
