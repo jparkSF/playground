9242bc12ff85a8ad2f7c22dc52bd9db9edbb3d79
jpark830@me.com <jpark830@me.com>
2018-10-11 18:52:47 -0700

    find dups

---
+++ w2/coding/findDups.js
@@ -0,0 +1,28 @@
+let findDups = array => {
+  hash = {}
+
+  array.forEach(el => {
+    if (!hash[el]) {
+      hash[el] = 1
+
+    } else {
+      hash[el] += 1
+    }
+  })
+
+  // console.log(hash)
+  dups = []
+  for (var key in hash) {
+    if (hash[key] > 1) {
+      dups.push(parseInt(key))
+    }
+  }
+
+  console.log(dups)
+}
+
+
+
+array = [1, 2, 2, 2, 3, 4, 5, 6, 6, 6, 7, 8, 9]
+
+findDups(array);
