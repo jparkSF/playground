5a91bf41e32cad2002c1cf3f134bbaef7a39e54f
jpark830@me.com <jpark830@me.com>
2018-10-15 11:48:20 -0700

    add merge sort

---
+++ w3/algorithms/mergesort.js
@@ -0,0 +1,8 @@
+// Split the array into halves and merge them recursively 
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    // return once we hit an array with a single item
+    return arr
+  }
+
+
