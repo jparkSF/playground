92a57e650ca542056e2cf68d487824bbfa36edc3
jpark830@me.com <jpark830@me.com>
2018-10-16 11:48:42 -0700

    add merge sort

--- w3/algorithms/mergesort.js
+++ w3/algorithms/mergesort.js
@@ -5,4 +5,10 @@ function mergeSort(arr) {
     return arr
   }
 
+  const middle = Math.floor(arr.length / 2) // get the middle item of the array rounded down
+  const left = arr.slice(0, middle) // items on the left side
+  const right = arr.slice(middle) // items on the right side
+
+
+}
 
