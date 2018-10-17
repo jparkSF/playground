3f1d311f4d44e1e3444e253cbf6c4378bacb090e
jpark830@me.com <jpark830@me.com>
2018-10-17 11:49:47 -0700

    merge sort

--- w3/algorithms/mergesort.js
+++ w3/algorithms/mergesort.js
@@ -1,5 +1,5 @@
 // Split the array into halves and merge them recursively 
-function mergeSort(arr) {
+function mergeSort (arr) {
   if (arr.length === 1) {
     // return once we hit an array with a single item
     return arr
@@ -16,15 +16,20 @@ function mergeSort(arr) {
 }
 
 // compare the arrays item by item and return the concatenated result
-function merge(left, right) {
+function merge (left, right) {
   let result = []
   let indexLeft = 0
   let indexRight = 0
 
-  
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] < right[indexRight]) {
+      result.push(left[indexLeft])
+      indexLeft++
+    } else {
+      result.push(right[indexRight])
+      indexRight++
+    }
+  }
 
   return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
 }
-
-const list = [2, 5, 1, 3, 7, 2, 3, 8, 6, 3]
-console.log(mergeSort(list)) // [ 1, 2, 2, 3, 3, 3, 5, 6, 7, 8 ]
