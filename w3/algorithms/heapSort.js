8ec9a9851e54a01e78390a9be6f8e01ac2d75e20
jpark830@me.com <jpark830@me.com>
2018-10-18 11:50:54 -0700

    heapSort

---
+++ w3/algorithms/heapSort.js
@@ -0,0 +1,21 @@
+
+var array_length;
+/* to create MAX  array */
+function heap_root(input, i) {
+  var left = 2 * i + 1;
+  var right = 2 * i + 2;
+  var max = i;
+
+  if (left < array_length && input[left] > input[max]) {
+    max = left;
+  }
+
+  if (right < array_length && input[right] > input[max]) {
+    max = right;
+  }
+
+  if (max != i) {
+    swap(input, i, max);
+    heap_root(input, max);
+  }
+}
