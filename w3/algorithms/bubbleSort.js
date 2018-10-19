4b2f10580643c63b1a6b39dfce6cf357db505761
jpark830@me.com <jpark830@me.com>
2018-10-19 11:56:06 -0700

    bubble sort

--- w3/algorithms/bubbleSort.js
+++ w3/algorithms/bubbleSort.js
@@ -7,3 +7,15 @@ function swap(array, i, j) {
   array[i] = array[j];
   array[j] = temp;
 }
+
+// be careful: this is a very basic implementation which is nice to understand the deep principle of bubble sort (going through all comparisons) but it can be greatly improved for performances
+function bubbleSortBasic(array) {
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 1; j < array.length; j++) {
+      if (array[j - 1] > array[j]) {
+        swap(array, j - 1, j);
+      }
+    }
+  }
+  return array;
+}
