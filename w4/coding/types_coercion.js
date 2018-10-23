1d6a2eeaa203fb1a420f13f6b5c0ce656e678636
jpark830@me.com <jpark830@me.com>
2018-10-23 12:50:01 -0700

    types in JS

--- w4/coding/types_coercion.js
+++ w4/coding/types_coercion.js
@@ -4,3 +4,10 @@ typeof 'Hello'        // string
 typeof Math           // object
 typeof null           // object  !!
 typeof Symbol('Hi')   // symbol (New ES6)
+
+Boolean(null)         // false
+Boolean('hello')      // true 
+Boolean('0')          // true 
+Boolean(' ')          // true 
+Boolean([])           // true 
+Boolean(function () { }) // true
