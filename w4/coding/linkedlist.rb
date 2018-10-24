be7f3204f3811cf5a056ed8fc097134a08888c0d
jpark830@me.com <jpark830@me.com>
2018-10-24 14:16:41 -0700

    linkedlist

--- w4/coding/linkedlist.rb
+++ w4/coding/linkedlist.rb
@@ -10,3 +10,18 @@ def add(val)
   end
   current.next = Node.new(val, nil)
 end
+
+def delete (val)
+  current.next = @head
+  if current.val = val
+      @head = current.next
+  else
+    while (current.next != nil) && (current.next.val != val)
+      current = current.next
+    end
+    unless current.next == nil
+    current.next = current.next.next
+    end
+  end
+end
+
