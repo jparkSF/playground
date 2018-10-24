3b0aa4215be0f3bc70f058a929bb88110eb0d7ae
jpark830@me.com <jpark830@me.com>
2018-10-24 14:16:11 -0700

    linkedlist

--- w4/coding/linkedlist.rb
+++ w4/coding/linkedlist.rb
@@ -2,3 +2,11 @@ class LinkedList
   def initialize(val)
   @head = Node.new(val, nil)
 end
+
+def add(val)
+  current = @head
+  while current.next != nil
+        current = current.next
+  end
+  current.next = Node.new(val, nil)
+end
