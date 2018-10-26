35840130dcb765bf1d95c46b4571bb89fff17bb6
jpark830@me.com <jpark830@me.com>
2018-10-26 14:20:21 -0700

    validate BST

--- w4/coding/validateBST.js
+++ w4/coding/validateBST.js
@@ -26,4 +26,23 @@ class BST {
     return root;
   }
 
+  // Function to determine if given Binary Tree is a BST or not by keeping a
+  // valid range (starting from [MIN_VALUE, MAX_VALUE]) and keep shrinking
+  // it down for each node as we go down recursively
+  isBST(node, minKey, maxKey) {
+    // base case
+    if (node == null) {
+      return true;
+    }
+
+    // if node's value fall outside valid range
+    if (node.data < minKey || node.data > maxKey) {
+      return false;
+    }
+
+    // recursively check left and right subtrees with updated range
+    return this.isBST(node.left, minKey, node.data) &&
+      this.isBST(node.right, node.data, maxKey);
+  }
+
 }
