15ac8dc0e2a017c05c6458a7b939bb17d7941944
jpark830@me.com <jpark830@me.com>
2018-10-26 14:20:10 -0700

    validate BST

--- w4/coding/validateBST.js
+++ w4/coding/validateBST.js
@@ -5,3 +5,25 @@ class Node {
     this.right;
   }
 }
+
+class BST {
+  // Recursive function to insert an key into BST
+  insert(root, key) {
+    // if the root is null, create a new node an return it
+    if (root == null) {
+      return new Node(key);
+    }
+
+    // if given key is less than the root node, recurse for left subtree
+    if (key < root.data) {
+      root.left = this.insert(root.left, key);
+    }
+    // if given key is more than the root node, recurse for right subtree
+    else {
+      root.right = this.insert(root.right, key);
+    }
+
+    return root;
+  }
+
+}
