2c0227acb28aa6aba6925f1481ea055e0c265c7f
jpark830@me.com <jpark830@me.com>
2018-10-31 02:24:11 -0700

    longest palindrome

---
+++ w5/algorithm/longest_palindrome.rb
@@ -0,0 +1,12 @@
+def find_longest_palindrome(s)  
+  longest = ""  
+  0.upto s.length do |i|  
+   odd = palindrome_at_position(s, i, i)  
+   longest = odd if odd.length > longest.size  
+   # handle even length palindromes  
+   even = palindrome_at_position(s, i, i+1)  
+   longest = even if even.length > longest.size  
+  end  
+  longest  
+ end
+
