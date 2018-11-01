d035ede73bd03b6c16c65f6d4591f0af6f46c638
jpark830@me.com <jpark830@me.com>
2018-11-01 02:28:16 -0700

    minimum number of jumps

--- w5/algorithm/minNumOfJumps.js
+++ w5/algorithm/minNumOfJumps.js
@@ -11,3 +11,22 @@ function minNumberOfJumps(array) {
   return jumps[jumps.length - 1];
 }
 // O(n^2) time & O(n) space
+
+
+function minNumberOfJumps(array) {
+  if (array.length === 1) return 0;
+  let jumps = 0;
+  let maxReach = array[0];
+  let steps = array[0];
+  for (let i = 1; i < array.length - 1; i++) {
+    maxReach = Math.max(maxReach, i + array[i]);
+    steps -= 1;
+    if (steps === 0) {
+      jumps += 1;
+      steps = maxReach - i
+    }
+  }
+  return jumps + 1
+}
+
+// O(n) time & O(1) space
