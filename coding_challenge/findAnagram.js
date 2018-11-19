7a071a65dc91dbd2b753d0b8935afbcdc6d6d441
jpark830@me.com <jpark830@me.com>
2018-11-19 12:37:12 -0800

    find anagram

---
+++ coding_challenge/findAnagram.js
@@ -0,0 +1,46 @@
+/* Find min difference to make anagram
+Input: 2 arrays. arr1 = ["a", "be", "cde"], arr2 = ["bb", "bc", "efg"]
+output: [-1,1,3]
+*/
+
+function makeAnagram(arr1, arr2) {
+  let result = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    result.push(helperAnagram(arr1[i], arr2[i]));
+  }
+  return result;
+}
+
+function helperAnagram(s1, s2) {
+  if (s1.length != s2.length) {
+    return -1;
+  }
+
+  let hash = {};
+  let sum = 0;
+
+  for (let char of s1) {
+    if (!hash[char]) {
+      hash[char] = 1;
+    }
+    else {
+      hash[char] += 1;
+    }
+  }
+
+  for (let char of s2) {
+    if (hash[char]) {
+      hash[char] -= 1;
+    }
+  }
+
+
+  for (let val of Object.values(hash)) {
+    sum += val;
+  }
+
+
+  return sum;
+}
+console.log(makeAnagram(arr1 = ["a", "be", "cde"], arr2 = ["bb", "bc", "efg"]))
