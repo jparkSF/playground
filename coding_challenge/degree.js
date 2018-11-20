de2b45a354fc7a3c35ffc0181ebfa5dd651ebdd2
jpark830@me.com <jpark830@me.com>
2018-11-20 00:53:29 -0800

    add new file

---
+++ coding_challenge/degree.js
@@ -0,0 +1,17 @@
+/* Degree of an array.
+Input:
+Output: Min(2,5);
+*/
+
+function degree(arr) {
+  /*
+  min = Number.MAX_SAFE_INTEGER;
+  1) make counting hash and find max value
+  2) iterate thru arr, and find keys that hash[key] == value.
+  3) Iterate through the keys, for each key use indexOf and lastIndexOf
+  to find start and end
+  4) min = Math.min(min, end-start + 1)
+  5) return min
+  */
+
+}
