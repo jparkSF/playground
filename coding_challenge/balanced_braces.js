90e11510eb4b2f0cfbcb7bfabd91fac7306c50ca
jpark830@me.com <jpark830@me.com>
2018-12-02 17:57:03 -0800

    mod balanced braces

--- coding_challenge/balanced_braces.js
+++ coding_challenge/balanced_braces.js
@@ -13,26 +13,48 @@ let isBalanced = values => {
 }
 
 let validator = string => {
-  let stack = [];
-  let lib = {
-    '(': ')',
-    '{': '}',
-    '[': ']'
-  };
+  // let stack = [];
+  // let lib = {
+  //   '(': ')',
+  //   '{': '}',
+  //   '[': ']'
+  // };
 
-  for (let i = 0; i < string.length; i++) {
-    let curr_char = string[i];
-    if (lib[curr_char]) {
-      stack.push(curr_char);
-    }
-    else {
-      if (stack.length == 0 || lib[stack.pop()] !== curr_char)
-        return "NO";
-    }
-  }
+  // for (let i = 0; i < string.length; i++) {
+  //   let curr_char = string[i];
+  //   if (lib[curr_char]) {
+  //     stack.push(curr_char);
+  //   }
+  //   else {
+  //     if (stack.length == 0 || lib[stack.pop()] !== curr_char)
+  //       return "NO";
+  //   }
+  // }
 
-  return stack.length == 0 ? "YES" : "NO";
-  // return val;
+  // return stack.length == 0 ? "YES" : "NO";
+  // // return val;
+
+
+  
+    let obj = {
+      '{': '}',
+      '(': ')',
+      '[': ']'
+    }
+    let stack = []
+    for (let i = 0; i < string.length; i++) {
+      let cur = string.charAt(i)
+      if (obj[cur] != undefined) {
+        stack.push(obj[cur])
+      } else {
+        let temp = stack.pop()
+        if (cur != temp) {
+          return false
+        }
+      }
+
+    }
+    return stack.length == 0
 }
 
 console.log(isBalanced(["(())", "([)"]));
