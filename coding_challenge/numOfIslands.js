1834824bb3ae00b152a2c34b8e225983385ec8b0
jpark830@me.com <jpark830@me.com>
2018-12-02 17:49:19 -0800

    number of islands

--- coding_challenge/numOfIslands.js
+++ coding_challenge/numOfIslands.js
@@ -9,6 +9,7 @@ const numIslands = (grid) => {
     for (let j = 0; j < grid[0].length; j++) {
       if (grid[i][j] === "1") {
         count++
+       
         exploreLand(grid, i, j)
       }
     }
@@ -17,21 +18,35 @@ const numIslands = (grid) => {
 }
 
 const exploreLand = (grid, x, y) => {
+  console.log("==========================")
+  console.log(x,y)
+  console.log(grid)
+  console.log("==========================")
   if (grid[x][y] === "1") {
+    
     grid[x][y] = "0"
+    // top
     if (x > 0 && (grid[x - 1][y] === "1"))
       exploreLand(grid, x - 1, y)
+    // left
     if (y > 0 && (grid[x][y - 1] === "1"))
       exploreLand(grid, x, y - 1)
+    // botton
     if (x < grid.length - 1 && (grid[x + 1][y] === "1"))
       exploreLand(grid, x + 1, y)
+    // right
     if (y < grid[0].length - 1 && (grid[x][y + 1] === "1"))
       exploreLand(grid, x, y + 1)
+      
   }
+  
   return
 }
 
 
-let input = [["1", "1", "1", "1", "0"], ["1", "1", "0", "1", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "0", "0", "0"]]
-numIslands(input)
+let input1 = [["1", "1", "1", "1", "0"], ["1", "1", "0", "1", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "0", "0", "0"]]
+let input2 = [["1", "1", "1"], ["0", "1", "0"], ["1", "1", "1"]]
+let input3 = [["1", "1", "0", "0", "0"], ["1", "1", "0", "0", "0"], ["0", "0", "1", "0", "0"], ["0", "0", "0", "1", "1"]]
+
+console.log(numIslands(input3))
 
