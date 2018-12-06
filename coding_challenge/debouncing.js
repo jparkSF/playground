cd3d8f28c4382ceb12f7619e36a342b9e17de83e
jpark830@me.com <jpark830@me.com>
2018-12-05 20:03:33 -0800

    debouncer

--- coding_challenge/debouncing.js
+++ coding_challenge/debouncing.js
@@ -1,56 +1,39 @@
-function logger(){
-  // print the current date each time it is invoked
-  console.log(new Date().toString());
+let logger = () => {
+  console.log(new Date().toString())
 }
 
-// Emulating event triggered by users interaction that uses logger as event handler
-// function emulateUserEvent(){
-//   var interval = setInterval(logger, 100);
-
-//   // In order to avoid being called infinitely,
-//   // let's clear the interval after 2000ms this emulating end of user interaction
-//   setTimeout(function(){
-//     clearInterval(interval)
-//   },2000)
-// }
-
-// emulateUserEvent()
+let debounce = function (fn, wait) {
+  let timeout;
 
 
-
-// Debouncing
-
-function debounce(fn, wait){
-  var timeout;
-  var debounced = function(){
-    var args = arguments, context = this;
-
-    function later(){
-      fn.call(context, args)
+  var debounced = function () {
+    let later = () => {
+      fn.call()
     }
 
     if (timeout) {
       clearTimeout(timeout)
     }
+
     timeout = setTimeout(later, wait)
   }
 
   return debounced
 }
-// get the debounced version of logger
-var debouncedLogger = debounce(logger, 500)
 
-// emulating event triggered by user interaction that uses debouncedLogger as event handler
-function emulateDebouncedUserEvent(){
+var debouncedLogger = debounce(logger, 400)
+
+let emulateDebouncedUserEvent = () => {
   var interval = setInterval(debouncedLogger, 100)
 
-  // in order to avoid being called infinitely,
-  // lets clear the interval after 2000ms this emulating end of user interaction
 
-  setTimeout(function(){
+  setTimeout(function () {
     clearInterval(interval)
   }, 2000)
 }
 
+// debouncedLogger()
+
 
 emulateDebouncedUserEvent()
+
